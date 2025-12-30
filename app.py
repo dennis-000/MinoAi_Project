32bd55d40b2a92641542305796f2ecc576c85f0f
Dennis <odennisasiedu@gmail.com>
2025-12-30 18:28:37 +0000

    Final

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def load_model():
 
 package, error_msg = load_model()
 
-# Sidebar - Friendly & Fun
+# Sidebar
 with st.sidebar:
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png", width=120)
     st.markdown("---")
